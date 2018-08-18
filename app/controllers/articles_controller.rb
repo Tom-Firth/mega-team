8cc3034e1620885663030934160fbf16c7d91dae
Tom-Firth <tom1.webdev@gmail.com>
2018-08-19 01:21:18 +0200

    mega-team creation

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_article)
+    @article.user = current_user
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(params_article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private 
+
+  def params_article
+    params.require(:article).permit(:title, :body)
+  end
+end
